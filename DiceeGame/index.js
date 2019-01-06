719d077c12b9cb669751981ca00a654b2ef78b46
jaydeninoz <jaydeninoz@gmail.com>
2019-01-06 12:49:03 +0700

    Dicee Game page added

---
+++ DiceeGame/index.js
@@ -0,0 +1,16 @@
+
+document.querySelector(".play").onclick = function(){
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    document.querySelector(".img1").setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+    document.querySelector(".img2").setAttribute("src", "/images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🥊 Player 1 wins";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 wins 🥊";
+    } else {
+        document.querySelector("h1").innerHTML = " 🤦 Drawwww!!! 🤦";
+    }
+}
