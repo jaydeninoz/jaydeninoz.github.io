19b84049486d026410c30e38c613d1b8bafe9b70
jaydeninoz <jaydeninoz@gmail.com>
2019-01-06 13:20:03 +0700

    fix index re-add

--- DiceeGame/script.js
+++ DiceeGame/script.js
@@ -1,11 +1,11 @@
 
 document.querySelector(".play").onclick = function(){
     var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-    var sourceRandomNumber1 = "/images/dice" + randomNumber1 + ".png";
+    var sourceRandomNumber1 = "images/dice" + randomNumber1 + ".png";
     document.querySelector(".img1").setAttribute("src", sourceRandomNumber1);
 
     var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-    var sourceRandomNumber2 = "/images/dice" + randomNumber2 + ".png";
+    var sourceRandomNumber2 = "images/dice" + randomNumber2 + ".png";
     document.querySelector(".img2").setAttribute("src", sourceRandomNumber2);
 
     if (randomNumber1 > randomNumber2) {
